acb99d7144a23ada0b6c48b1ff93528484e854fd
Kylen Solvik <kysolvik@gmail.com>
2018-03-25 08:32:23 -0400

    Added preprocess and train functions
    ... Need some work,  but it's a good start

--- reservoir-id-cnn/train/train.py
+++ reservoir-id-cnn/train/train.py
@@ -42,3 +42,77 @@ def get_unet(img_rows, img_cols, img_bands):
     model.compile(optimizer=Adam(lr=1e-5), loss=dice_coef_loss, metrics=[dice_coef])
 
     return model
+
+def preprocess(imgs):
+    imgs_p = np.ndarray((imgs.shape[0], img_rows, img_cols), dtype=np.uint8)
+    for i in range(imgs.shape[0]):
+        imgs_p[i] = resize(imgs[i], (img_cols, img_rows), preserve_range=True)
+
+    imgs_p = imgs_p[..., np.newaxis]
+    return imgs_p
+
+
+def train_and_predict():
+    print('-'*30)
+    print('Loading and preprocessing train data...')
+    print('-'*30)
+    imgs_train, imgs_mask_train = load_train_data()
+
+    imgs_train = preprocess(imgs_train)
+    imgs_mask_train = preprocess(imgs_mask_train)
+
+    imgs_train = imgs_train.astype('float32')
+    mean = np.mean(imgs_train)  # mean for data centering
+    std = np.std(imgs_train)  # std for data normalization
+
+    imgs_train -= mean
+    imgs_train /= std
+
+    imgs_mask_train = imgs_mask_train.astype('float32')
+    imgs_mask_train /= 255.  # scale masks to [0, 1]
+
+    print('-'*30)
+    print('Creating and compiling model...')
+    print('-'*30)
+    model = get_unet()
+    model_checkpoint = ModelCheckpoint('weights.h5', monitor='val_loss', save_best_only=True)
+
+    print('-'*30)
+    print('Fitting model...')
+    print('-'*30)
+    model.fit(imgs_train, imgs_mask_train, batch_size=32, nb_epoch=20, verbose=1, shuffle=True,
+              validation_split=0.2,
+              callbacks=[model_checkpoint])
+
+    print('-'*30)
+    print('Loading and preprocessing test data...')
+    print('-'*30)
+    imgs_test, imgs_id_test = load_test_data()
+    imgs_test = preprocess(imgs_test)
+
+    imgs_test = imgs_test.astype('float32')
+    imgs_test -= mean
+    imgs_test /= std
+
+    print('-'*30)
+    print('Loading saved weights...')
+    print('-'*30)
+    model.load_weights('weights.h5')
+
+    print('-'*30)
+    print('Predicting masks on test data...')
+    print('-'*30)
+    imgs_mask_test = model.predict(imgs_test, verbose=1)
+    np.save('imgs_mask_test.npy', imgs_mask_test)
+
+    print('-' * 30)
+    print('Saving predicted masks to files...')
+    print('-' * 30)
+    pred_dir = 'preds'
+    if not os.path.exists(pred_dir):
+        os.mkdir(pred_dir)
+    for image, image_id in zip(imgs_mask_test, imgs_id_test):
+        image = (image[:, :, 0] * 255.).astype(np.uint8)
+        imsave(os.path.join(pred_dir, str(image_id) + '_pred.png'), image)
+        
+    return None
