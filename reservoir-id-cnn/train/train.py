038e53ced7f712dce13673e73545750d96e62dc7
Kylen Solvik <kysolvik@gmail.com>
2018-04-15 19:02:06 +0000

    Train working ok, but either needs more data or a lot of tuning.

--- reservoir-id-cnn/train/train.py
+++ reservoir-id-cnn/train/train.py
@@ -12,16 +12,25 @@ from keras.optimizers import Adam, SGD
 from keras.callbacks import ModelCheckpoint
 from keras import backend as K
 from skimage import io
-from keras.layers.advanced_activations import LeakyReLU
 
 K.set_image_data_format('channels_last')  # TF dimension ordering in this code
 
-RESIZE_ROWS = 256
-RESIZE_COLS = 256
+OG_ROWS = 500
+OG_COLS = 500
+# Original image dimensions
+
+RESIZE_ROWS = 400
+RESIZE_COLS = 400
 # Resized dimensions for training/testing.
+
 NUM_BANDS = 4
 # Number of bands in image.
-SMOOTH = 1e-1
+
+SMOOTH = 1.
+# Smoothing factor for jaccard_coef
+
+PRED_THRESHOLD = 0.5
+# Prediction threshold. > PRED_THRESHOLD will be classified as res.
 
 
 def jaccard_coef(y_true, y_pred, smooth=SMOOTH):
@@ -63,6 +72,21 @@ def jaccard_distance_loss(y_true, y_pred, smooth=SMOOTH):
     jac = (intersection + smooth) / (sum_ - intersection + smooth)
     return (1 - jac) * smooth
 
+
+def stretch_n(bands, lower_percent=5, higher_percent=95):
+    out = np.zeros_like(bands)
+    n = bands.shape[2]
+    for i in range(n):
+        a = 0  # np.min(band)
+        b = 1  # np.max(band)
+        c = np.percentile(bands[:, :, i], lower_percent)
+        d = np.percentile(bands[:, :, i], higher_percent)
+        t = a + (bands[:, :, i] - c) * (b - a) / (d - c)
+        t[t < a] = a
+        t[t > b] = b
+        out[:, :, i] = t
+
+    return out.astype(np.float32)
 
 def dice_coef(y_true, y_pred, smooth=SMOOTH):
     y_true_f = K.flatten(y_true)
@@ -121,7 +145,7 @@ def get_unet(img_rows, img_cols, nbands):
 
     model = Model(inputs=[inputs], outputs=[conv10])
 
-    model.compile(optimizer=Adam(lr=0.00001), loss=dice_coef_loss,
+    model.compile(optimizer=Adam(lr=1e-5), loss=dice_coef_loss,
                   metrics=[jaccard_coef, jaccard_coef_int, 'accuracy'])
 
     return model
@@ -135,7 +159,6 @@ def img_resize(imgs, nbands):
                                      (RESIZE_ROWS, RESIZE_COLS, nbands),
                                      preserve_range = True)
 
-    # imgs_p = imgs_p[..., np.newaxis]
     return imgs_p
 
 
@@ -146,15 +169,6 @@ def train_and_predict():
     imgs_train = np.load('./data/prepped/imgs_train.npy')
     imgs_mask_train = np.load('./data/prepped/imgs_mask_train.npy')
 
-#     null_masks = np.ones(imgs_mask_train.shape[0], dtype=bool)
-#     for i in range(imgs_mask_train.shape[0]):
-#         max_mask = np.max(imgs_mask_train[i,:,:])
-#         if max_mask < 255:
-#             null_masks[i] = 0
-#
-#     imgs_train = imgs_train[null_masks]
-#     imgs_mask_train = imgs_mask_train[null_masks]
-
     imgs_train = img_resize(imgs_train, 4)
     imgs_mask_train = img_resize(imgs_mask_train, 1)
 
@@ -162,8 +176,10 @@ def train_and_predict():
     mean = np.mean(imgs_train)  # mean for data centering
     std = np.std(imgs_train)  # std for data normalization
 
-    imgs_train -= mean
-    imgs_train /= std
+#     imgs_train -= mean
+#     imgs_train /= std
+    for i in imgs_train.shape[0]:
+        imgs_train[i] = stretch_n(imgs_train[i])
 
     imgs_mask_train = imgs_mask_train.astype('float32')
     imgs_mask_train /= 255.  # scale masks to [0, 1]
@@ -179,7 +195,7 @@ def train_and_predict():
     print('Fitting model...')
     print('-'*30)
 
-    model.fit(imgs_train, imgs_mask_train, batch_size=4, epochs=20,
+    model.fit(imgs_train, imgs_mask_train, batch_size=8, epochs=30,
               verbose=1, shuffle=True, validation_split=0.2,
               callbacks=[model_checkpoint])
 
@@ -187,11 +203,14 @@ def train_and_predict():
     print('Loading and preprocessing test data...')
     print('-'*30)
     imgs_test = np.load('./data/prepped/imgs_test.npy')
+    imgs_mask_test = np.load('./data/prepped/imgs_mask_test.npy')
     imgs_test = img_resize(imgs_test, 4)
 
     imgs_test = imgs_test.astype('float32')
-    imgs_test -= mean
-    imgs_test /= std
+    for i in imgs_test.shape[0]:
+        imgs_test[i] = stretch_n(imgs_test[i])
+#     imgs_test -= mean
+#     imgs_test /= std
 
     print('-'*30)
     print('Loading saved weights...')
@@ -210,14 +229,45 @@ def train_and_predict():
 
     np.save('{}pred_test_masks.npy'.format(predict_dir), pred_test_masks)
     test_img_names = open('./data/prepped/test_names.csv').read().splitlines()
+
+    # For calculating total error metrics
+    total_res_pixels = 0
+    total_true_positives = 0
+    total_false_positives = 0
     for i in range(pred_test_masks.shape[0]):
         pred_mask = pred_test_masks[i]
+        true_mask = imgs_mask_test[i]
         print(np.min(pred_mask), np.max(pred_mask))
-        pred_mask = pred_mask > 0.51
+        pred_mask = transform.resize(pred_mask,
+                                     (OG_ROWS, OG_COLS, 1),
+                                     preserve_range = True)
         pred_mask = (pred_mask[:, :, 0] * 255.).astype(np.uint8)
+
+        # Save predicted masks
         pred_mask_filename = test_img_names[i].replace('og.tif', 'predmask.png')
         io.imsave('{}{}'.format(predict_dir, pred_mask_filename), pred_mask)
+
+        # Save predicted and actual masks side by side
+        both_mask_filename = test_img_names[i].replace('og.tif', 'bothmask.png')
+        both_mask = np.zeros((OG_ROWS, OG_COLS * 2 + 10), dtype=np.uint8)
+        both_mask[0:OG_ROWS, 0:OG_COLS] = pred_mask
+        both_mask[0:OG_ROWS, (OG_COLS + 10):(OG_COLS * 2 + 10)] = true_mask
+        io.imsave('{}{}'.format(predict_dir, both_mask_filename), both_mask)
+
+        # Calculate basic error
+        pred_mask = 255*(pred_mask > (PRED_THRESHOLD*255)) # move this to earlier
+        total_res_pixels += np.sum(true_mask == 255)
+        total_true_positives += np.sum((true_mask == 255) * (pred_mask == 255))
+        total_false_positives += np.sum((true_mask == 0) * (pred_mask == 255))
+
         i += 1
+    print('Total Res Pixels: {}'.format(total_res_pixels))
+    print('Total True Pos: {} ({})'
+          .format(total_true_positives,
+                  total_true_positives/total_res_pixels))
+    print('Total False Pos: {} ({})'
+          .format(total_false_positives,
+                  total_false_positives/total_res_pixels))
 
     return
 
