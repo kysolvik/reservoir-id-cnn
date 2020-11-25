fef0c90b69fedc9476e09cd512130993b5965c12
Kylen Solvik <kysolvik@gmail.com>
2020-11-25 18:45:04 +0000

    Small tweaks to metrics calculations

--- reservoir-id-cnn/train/calc_metrics.py
+++ reservoir-id-cnn/train/calc_metrics.py
@@ -4,20 +4,20 @@ from skimage import transform
 
 # test_npy_path = sys.argv[1]
 # pred_npy_path = sys.argv[2]
-pred_npy_path = './logs/v23_fulltrain_good_68f1/predict/pred_test_masks.npy'
+pred_npy_path = 'data/predict/pred_test_masks.npy'
 test_npy_path = 'data/prepped/imgs_mask_test.npy'
 
 pred = np.load(pred_npy_path)
 pred = pred[:,:,:,0]
 pred = transform.resize(pred, (pred.shape[0], 500, 500),
                         preserve_range=True)
-pred[pred>0.5] = 255
-pred[pred<0.5] = 0
+pred[pred>0.25] = 255
+pred[pred<0.25] = 0
 pred = pred.astype(np.uint8)
 test = np.load(test_npy_path)
 
-test = np.delete(test, -11,axis=0) # Remove HUGE reservoir)
-pred = np.delete(pred, -11,axis=0)
+# test = np.delete(test, -11,axis=0) # Remove HUGE reservoir)
+# pred = np.delete(pred, -11,axis=0)
 
 true_pos = np.sum((pred==255)*(test==255))
 false_pos = np.sum((pred==255)*(test==0))
