d74a16effa64e88d952f70b4d32d38857d32c74c
Kylen Solvik <kysolvik@gmail.com>
2020-12-01 16:16:50 +0000

    Added script for copying inputs to local SSD

---
+++ reservoir-id-cnn/predict/copy_inputs.sh
@@ -0,0 +1,4 @@
+gsutil -m cp -r gs://res-id/ee_exports/sentinel /mnt/disks/pred_data/
+gsutil -m cp -r gs://res-id/ee_exports/sentinel1_10m_v2 /mnt/disks/pred_data/
+gsutil -m cp -r gs://res-id/ee_exports/sentinel2_20m /mnt/disks/pred_data/
+
