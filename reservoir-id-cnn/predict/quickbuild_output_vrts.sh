8ece0c311f541efb09bc8ec1082c654e3c189c7c
Kylen Solvik <kysolvik@gmail.com>
2020-12-03 12:54:54 +0000

    fixed output vrt build

--- reservoir-id-cnn/predict/quickbuild_output_vrts.sh
+++ reservoir-id-cnn/predict/quickbuild_output_vrts.sh
@@ -1,4 +1,3 @@
-d=$1
-gdalbuildvrt temp/${d}.vrt ${d}/*.tif
-gdal_translate -co "COMPRESS=LZW" temp/${d}.vrt temp/${d}.tif
+gdalbuildvrt temp/out.vrt /mnt/disks/pred_data/out/*.tif
+gdal_translate -co "COMPRESS=LZW" temp/out.vrt temp/out.tif
 
