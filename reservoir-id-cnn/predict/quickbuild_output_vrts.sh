2a39cff94b3b457a985cc7c885ac990a939c625f
Kylen Solvik <kysolvik@gmail.com>
2021-01-26 23:23:34 +0000

    Add wrapper to build multiple outputvrts

--- reservoir-id-cnn/predict/quickbuild_output_vrts.sh
+++ reservoir-id-cnn/predict/quickbuild_output_vrts.sh
@@ -1,3 +1,4 @@
-gdalbuildvrt temp/out.vrt /mnt/disks/pred_data/out/*.tif
-gdal_translate -co "COMPRESS=LZW" temp/out.vrt temp/out.tif
+num_start=${1}
+gdalbuildvrt temp/$1.vrt /mnt/disks/pred_data/out/pred_$1*.tif
+gdal_translate -co "COMPRESS=LZW" temp/$1.vrt temp/$1.tif
 
