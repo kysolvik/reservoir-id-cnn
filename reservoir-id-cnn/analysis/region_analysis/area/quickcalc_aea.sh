a66c72a3afe6461c33393262b2d7e9e94c9701e4
Kylen Solvik <kysolvik@gmail.com>
2021-01-27 10:42:50 -0700

    Quickcalc script for region area calculations

---
+++ reservoir-id-cnn/analysis/region_analysis/area/quickcalc_aea.sh
@@ -0,0 +1,3 @@
+python3 calc_areas_regions.py ~/research/reservoirs/data/predict/v3/aea/full_aea_v3.tif ./biomes_raster_aea.tif data/biomes_sizes_aea.csv
+python3 calc_areas_regions.py ~/research/reservoirs/data/predict/v3/aea/full_aea_v3.tif ./states_raster_aea.tif data/states_sizes_aea.csv
+python3 calc_areas_regions.py ~/research/reservoirs/data/predict/v3/aea/full_aea_v3.tif ./watersheds_raster_aea.tif data/watersheds_sizes_aea.csv
